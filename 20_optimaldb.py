5cf65e8fcdd662bc70b327da0d19cf0246756060
Damian Nowakowski <damnowgry@gmail.com>
2024-04-17 09:27:21 +0200

    cleanup

--- 20_optimaldb.py
+++ 20_optimaldb.py
@@ -1,14 +1,6 @@
 from apihelper import sendAnswer
 from apihelper import getTokenAndTask
 
-import requests
-import os
-import json
-from dotenv import load_dotenv
-
-load_dotenv()
-key = os.getenv("RENDERFORM_API_KEY")
-
 token, task = getTokenAndTask("optimaldb", None, None)
 if task != None:
     with open("friends_compressed.json", 'r', encoding='utf-8') as file:
